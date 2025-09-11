5ebec38743332c8ff4d330040c2325efa90326b8
Georgi Andreev <georgi.v.andreev@gmail.com>
2025-09-11 17:15:42 +0300

    first test final code3

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,14 @@
 output "alb_dns_name" {
-  value = aws_lb.alb.dns_name
-}
-
-output "web1_public_ip" {
-  value = aws_instance.web1.public_ip
-}
-
-output "web2_public_ip" {
-  value = aws_instance.web2.public_ip
+  description = "DNS name of the ALB"
+  value       = aws_lb.web_alb.dns_name
 }
 
 output "rds_master_endpoint" {
-  value = aws_db_instance.master.endpoint
+  description = "Endpoint of the RDS master"
+  value       = aws_db_instance.db_master.endpoint
 }
 
-output "rds_slave_endpoint" {
-  value = aws_db_instance.slave.endpoint
+output "rds_replica_endpoint" {
+  description = "Endpoint of the RDS replica"
+  value       = aws_db_instance.db_replica.endpoint
 }
