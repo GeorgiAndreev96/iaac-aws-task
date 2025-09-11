ed2bfce5fb11a6a216dead71b211d2ac5ad35bb9
Georgi Andreev <georgi.v.andreev@gmail.com>
2025-09-11 17:26:50 +0300

    first test final code5

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,8 +56,6 @@ resource "aws_db_instance" "db_replica" {
   identifier              = "db-iaac-task2"
   engine                  = "mysql"
   instance_class          = "db.t3.micro"
-  username                = var.rds_username
-  password                = var.rds_password
   allocated_storage       = 20
   db_subnet_group_name    = var.db_subnet_group_name
   vpc_security_group_ids  = var.vpc_security_group_ids
@@ -71,20 +69,48 @@ resource "aws_db_instance" "db_replica" {
 }
 
 ############################################################
-# APPLICATION LOAD BALANCER
+# NEW TARGET GROUP FOR ALB
+############################################################
+resource "aws_lb_target_group" "web_tg" {
+  name     = "TG-iaac-task-new"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  target_type = "instance"
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+
+  tags = {
+    Name = "TG-iaac-task-new"
+  }
+}
+
+############################################################
+# NEW APPLICATION LOAD BALANCER
 ############################################################
 resource "aws_lb" "web_alb" {
-  name               = "aws-iaac-alb"
+  name               = "aws-iaac-alb-new"
   internal           = false
   load_balancer_type = "application"
   security_groups    = var.vpc_security_group_ids
   subnets            = var.subnet_ids
 
   tags = {
-    Name = "aws-iaac-alb"
+    Name = "aws-iaac-alb-new"
   }
 }
 
+############################################################
+# LISTENER FOR THE ALB
+############################################################
 resource "aws_lb_listener" "web_listener" {
   load_balancer_arn = aws_lb.web_alb.arn
   port              = 80
@@ -92,21 +118,22 @@ resource "aws_lb_listener" "web_listener" {
 
   default_action {
     type             = "forward"
-    target_group_arn = "arn:aws:elasticloadbalancing:eu-north-1:851725313439:targetgroup/TG-iaac-task/cb1528ee8919603b"
+    target_group_arn = aws_lb_target_group.web_tg.arn
   }
 }
 
 ############################################################
-# REGISTER EC2 INSTANCES WITH TARGET GROUP
+# REGISTER EC2 INSTANCES WITH NEW TARGET GROUP
 ############################################################
 resource "aws_lb_target_group_attachment" "web1_attach" {
-  target_group_arn = "arn:aws:elasticloadbalancing:eu-north-1:851725313439:targetgroup/TG-iaac-task/cb1528ee8919603b"
+  target_group_arn = aws_lb_target_group.web_tg.arn
   target_id        = aws_instance.web1.id
   port             = 80
 }
 
 resource "aws_lb_target_group_attachment" "web2_attach" {
-  target_group_arn = "arn:aws:elasticloadbalancing:eu-north-1:851725313439:targetgroup/TG-iaac-task/cb1528ee8919603b"
+  target_group_arn = aws_lb_target_group.web_tg.arn
   target_id        = aws_instance.web2.id
   port             = 80
 }
+
